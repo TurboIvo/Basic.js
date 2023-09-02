bbc792f064ad697d5b5eebfee8439215bd08d789
TurboIvo <143627229+TurboIvo@users.noreply.github.com>
2023-09-02 23:44:01 +0300

    Update Basic.js

--- Basic.js
+++ Basic.js
@@ -1,25 +1,27 @@
 function letters(input) {
-  let firstLetter = input[0];
-  let secondLetter = input[1];
-  let thirdLetter = input[2];
-  let forthLetter = input[3];
-  let fifthLetter = input[4];
-  let sixthLetter = input[5];
-  let seventhLetter = input[6];
-  let eightLetter = input[7];
+    let firstLetter = input[0];
+    let secondLetter = input[1];
+    let thirdLetter = input[2];
+    let forthLetter = input[3];
+    let fifthLetter = input[4];
+    let sixthLetter = input[5];
+    let seventhLetter = input[6];
+    let eightLetter = input[7];
+    console.log(firstLetter);
+  }
+  letters(["a", "b", "c", "d", "e", "f", "g", "h"]);
   
-}
-letters(["a", "b", "c", "d", "e", "f", "g", "h"]);
-
-function numbers(input) {
-  let firstNumber = input[0];
-  let secondNumber = input[1];
-  let thirdNumber = input[2];
-  let forthNumber = input[3];
-  let fifthNumber = input[4];
-  let sixthNumber = input[5];
-  let seventhNumber = input[6];
-  let eightNumberr = input[7];
-}
-numbers(["1", "2", "3", "4", "5", "6", "7", "8"]);
-console.log(firstLetter, secondNumber);
+  function numbers(input) {
+    let firstNumber = input[0];
+    let secondNumber = input[1];
+    let thirdNumber = input[2];
+    let forthNumber = input[3];
+    let fifthNumber = input[4];
+    let sixthNumber = input[5];
+    let seventhNumber = input[6];
+    let eightNumberr = input[7];
+    console.log(secondNumber);
+  }
+  numbers(["1", "2", "3", "4", "5", "6", "7", "8"]);
+  
+  
