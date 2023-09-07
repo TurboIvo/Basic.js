9253f5a33fd5154fd4c6a2f85c512303d9d5046c
TurboIvo <143627229+TurboIvo@users.noreply.github.com>
2023-09-08 00:03:31 +0300

    Create petShop.js

---
+++ petShop.js
@@ -0,0 +1,7 @@
+function petShop(input) {
+    let dogFood = input[0];
+    let catFood = input[1];
+    let petFoodSum = dogFood * 2.5 + catFood * 4;
+    console.log(petFoodSum + " lv.");
+}
+petShop(["5", "4"]);
