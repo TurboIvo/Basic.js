1a878b4b5a76d8b8c73332f9432ce297b97e2f58
TurboIvo <143627229+TurboIvo@users.noreply.github.com>
2025-04-16 23:33:59 +0300

    Add files via upload

---
+++ exercise1.js
@@ -0,0 +1,5 @@
+function exercise1 (name, midName, lastName) {
+   console.log('Hello' + ' ' + name + ' ' + midName + ' ' + lastName); 
+}
+
+exercise1('John', 'Johnny', '"The John"');
---
+++ exercise2.js
@@ -0,0 +1,5 @@
+function movePiece(piece, fromSquare, toSquare) {
+    console.log(`${piece} moves from ${fromSquare} to ${toSquare}`);
+  }
+  
+  movePiece("Knight", "g1", "f3");
